320a3536eae0426fb1ebf408c7b346398b5c593e
Rafael Evaristo <rafael.evaristo@vision-box.com>
2024-01-09 00:23:21 +0000

    Adicionado mais um ingrediente ao sumo.

--- exercicioum/src/main/java/com/mycompany/exercicioum/Exercicioum.java
+++ exercicioum/src/main/java/com/mycompany/exercicioum/Exercicioum.java
@@ -19,11 +19,11 @@ public class Exercicioum {
         // Criar instâncias
         BebidaAlc cerveja = new BebidaAlc("Cerveja", 150, 5.0, 330, 5.0);
         Agua aguaMineral = new Agua("Água Mineral", 0, 2.0, 500, 7.0);
-        Sumo sucoLaranja = new Sumo("Suco de Laranja", 100, 3.5, 250, Arrays.asList("Laranja"));
+        Sumo sumoPinacolada = new Sumo("Sumo de Pinacolada", 100, 3.5, 250, Arrays.asList("Ananás","Coco"));
 
         // Exibir informações graficamente
         JOptionPane.showMessageDialog(null, cerveja.toString());
         JOptionPane.showMessageDialog(null, aguaMineral.toString());
-        JOptionPane.showMessageDialog(null, sucoLaranja.toString());
+        JOptionPane.showMessageDialog(null, sumoPinacolada.toString());
     }
 }

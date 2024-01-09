fe1cb82993f05990310feef224d3eb6620bf179f
Rafael Evaristo <rafael.evaristo@vision-box.com>
2024-01-09 00:56:48 +0000

    WIP: Added initial form.

---
+++ exerciciodois/src/main/java/com/mycompany/exerciciodois/Exerciciodois.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exerciciodois;
+
+/**
+ *
+ * @author Rafael.Evaristo
+ */
+public class Exerciciodois {
+
+    public static void main(String[] args) {
+        new TemperatureConverterForm().setVisible(true);
+    }
+}
